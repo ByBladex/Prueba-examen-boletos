f9fb33320e42d98dab16455300ca53fc90a3a44a
ByBladex <49193961+ByBladex@users.noreply.github.com>
2021-05-04 20:38:02 +0200

    100% Consola

--- Practica Examen Boletos/app.js
+++ Practica Examen Boletos/app.js
@@ -1,4 +1,45 @@
-let servicio = new Servicio;
+let servicio = new Servicio();
 
-let btnGenerar = document.getElementById("btnGenerar");
-btnGenerar.addEventListener('click', servicio.generarBoleto);
+function main(){
+    let btnGenerar = document.getElementById('btnGenerar');
+    btnGenerar.addEventListener('click', generarBoleto);
+
+    let btnMostrar = document.getElementById('btnMostrar');
+    btnMostrar.addEventListener('click',mostrarBoletos);
+
+    let btnMostrarPares = document.getElementById('btnMostrarPares');
+    btnMostrarPares.addEventListener('click',mostrarBoletosPares);
+
+    let btnMostrarImpares = document.getElementById('btnMostrarImpares');
+    btnMostrarImpares.addEventListener('click',mostrarBoletosImpares);
+}
+
+function generarBoleto(){
+    servicio.generarBoleto(generarIdRnd(0,100000), Date.now);
+}
+
+function generarIdRnd(min, max){
+    return random = Math.round(Math.random() * (max - min) + min);
+}
+
+function mostrarBoletos(){
+    servicio.boletos.forEach(element => {
+        console.log(element.id);
+    });
+}
+
+function mostrarBoletosPares(){
+    servicio.boletos.forEach(element => {
+        if(element.id%2==0)
+            console.log(element.id);
+    });
+}
+
+function mostrarBoletosImpares(){
+    servicio.boletos.forEach(element => {
+        if(element.id%2!=0)
+            console.log(element.id);
+    });
+}
+
+window.addEventListener('load', main);
--- Practica Examen Boletos/modelo.js
+++ Practica Examen Boletos/modelo.js
@@ -3,46 +3,15 @@ class Boleto {
         this.id = id;
         this.fechaEmision = fechaEmision;
     }
-
-    esPar() {
-        let esPar = false;
-        if (this.id % 2 == 0)
-            esPar = true;
-        else
-            esPar = false;
-        return esPar;
-    }
 }
 
 class Servicio {
-
+    
     constructor() {
-        var arrayBoletos = [new Boleto("15651", "22/04/2021"), new Boleto("65451", "22/04/2021")];
+        this.boletos = [new Boleto('32174', '2021-04-25')];
     }
 
-    generarBoleto() {
-        this.arrayBoletos.push(new Boleto("15258", "22/04/2021"))
-    }
-
-    mostrarBoletos() {
-        for (let i = 0; i < this.arrayBoletos.length; i++) {
-            const boleto = arrayBoletos[i];
-            console.log(boleto.id);
-        }
-    }
-
-    mostrarBoletosPares() {
-        for (let i = 0; i < arrayBoletos.length; i++) {
-            const boleto = arrayBoletos[i];
-            if (boleto.isPar())
-                console.log(boleto.id);
-        }
-    }
-    mostrarBoletosImpares() {
-        for (let i = 0; i < arrayBoletos.length; i++) {
-            const boleto = arrayBoletos[i];
-            if (!boleto.isPar())
-                console.log(boleto.id);
-        }
+    generarBoleto(id, fecha) {
+        this.boletos.push(new Boleto(id, fecha));
     }
 }
